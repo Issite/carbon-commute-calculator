3662b91e705b1c80bf1daa1ad4524f05aa50c10e
Caleb Hazelton <caleb.hazelton@gmail.com>
2025-11-17 14:50:01 -0500

    trip attributes added

--- src/trip.py
+++ src/trip.py
@@ -1,10 +1,11 @@
 # Class containing route object, vehicle object, and methods to calculate total emissions for a trip.
 class Trip:
-    def __init__(self, route, vehicle, frequency, occurance):
+    def __init__(self, route, vehicle, frequency=0, occurance=1, passengers=1):
         self.route = route
         self.vehicle = vehicle
         self.frequency = frequency
         self.occurance = occurance
+        self.passengers = passengers
         self.total_emissions = self.calculate_emissions()
 
     def calculate_emissions(self):
